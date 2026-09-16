52652da04b7c7ba2d1ebe343702ce7e5b7249071
T.J. Pelon <tjpelon@icloud.com>
2026-09-15 22:46:42 -0400

    Add files via upload

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,14 @@
+function isPrime(n){
+    if (n === 1){
+        return false
+    }
+    for (let i=2; i <= n/2; i++){
+        if (n % i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+module.exports =  { isPrime }
+
